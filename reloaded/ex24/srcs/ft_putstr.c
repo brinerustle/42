723ffc6b64d836043a0c3126c8fc7d3e4adbaf85
bkrueger <bkrueger@student.42barcelona.com>
2023-09-08 14:33:27 +0200

    added all

---
+++ reloaded/ex24/srcs/ft_putstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkrueger <bkrueger@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 13:42:11 by bkrueger          #+#    #+#             */
+/*   Updated: 2023/09/08 14:01:23 by bkrueger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i ++;
+	}
+}
+
+int	main(void)
+{
+	ft_putstr("this is a string");
+	return (0);
+}
