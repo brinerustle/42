f08b6fc30aeb902c0fdd9f9bc9453fb1cc54d764
bkrueger <bkrueger@student.42barcelona.com>
2023-09-11 22:55:47 +0200

    two more exercises

--- reloaded/ex16/ft_str_len.c
+++ reloaded/ex16/ft_str_len.c
@@ -1,43 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*   ft_str_len.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bkrueger <bkrueger@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/11 22:07:36 by bkrueger          #+#    #+#             */
-/*   Updated: 2023/09/11 22:11:02 by bkrueger         ###   ########.fr       */
+/*   Created: 2023/09/11 22:12:31 by bkrueger          #+#    #+#             */
+/*   Updated: 2023/09/11 22:51:35 by bkrueger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_sqrt(int nb)
+int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (nb < 0)
+	while (str[i] != '\0')
 	{
-		return (0);
+		i ++;
 	}
-	else
-	{
-		while (i < 46341)
-		{
-			if (nb == i * i)
-			{
-				return (i);
-			}
-			i ++;
-		}
-		return (0);
-	}
+	return (i);
 }
-/*
-int	main (void)
+
+int	main(void)
 {
-	int nb = -2147483647;
-	printf("square root of %d is %d\n", nb, ft_sqrt(nb));
+	char	*c;
+
+	c = "abcdefghijklmnopqrstuvwxyz";
+	printf("the string \"%s\" is %d characters long.\n", c, ft_strlen(c));
 	return (0);
-}*/
+}
